d0f4e14fd441d06dc5291b904c56fc0dbf9cc720
Daniel <daniel.agostinho@hotmail.com>
2020-12-26 11:43:05 -0300

    13.3. Adicionando botão de seleção

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {PessoasPesquisaComponent} from './pessoas-pesquisa/pessoas-pesquisa.comp
 import {LancamentoCadastroComponent} from './lancamento-cadastro/lancamento-cadastro.component';
 import {InputTextareaModule} from 'primeng/inputtextarea';
 import {CalendarModule} from 'primeng/calendar';
+import {SelectButtonModule} from 'primeng/selectbutton';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import {CalendarModule} from 'primeng/calendar';
     ButtonModule,
     TableModule,
     TooltipModule,
-    CalendarModule
+    CalendarModule,
+    SelectButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
